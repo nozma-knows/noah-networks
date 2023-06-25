09657f0b11a0a49264393ccd54a76c7fa2a10ba7
Noah Milberger <n.milberger@gmail.com>
2023-06-25 08:24:45 -0400

    still working on fix.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,32 +1,40 @@
-import { getClient } from "@/lib/client";
-import { ProjectsQuery } from "@/components/graph";
+// import { getClient } from "@/lib/client";
+// import { ProjectsQuery } from "@/components/graph";
 
 import Page from "@/components/ui/pages/Page";
-import { ProjectsGrid } from "@/components/feature-projects";
-import { Project } from "@/__generated__/graphql";
+// import { ProjectsGrid } from "@/components/feature-projects";
+// import { Project } from "@/__generated__/graphql";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-export const metadata = {
-  title: "Noah Networks | Projects",
-  description: "Noah Networks projects page.",
-};
+// export const metadata = {
+//   title: "Noah Networks | Projects",
+//   description: "Noah Networks projects page.",
+// };
 
-// Content
-const title = "Projects.";
+// // Content
+// const title = "Projects.";
 
-export default async function Projects() {
-  // Grab projects
-  const { data }: { data: { projects: Project[] } } = await getClient().query({
-    query: ProjectsQuery,
-  });
+// export default async function Projects() {
+//   // Grab projects
+//   const { data }: { data: { projects: Project[] } } = await getClient().query({
+//     query: ProjectsQuery,
+//   });
 
+//   return (
+//     <Page>
+//       <div className="flex flex-col w-full h-full gap-8">
+//         <h1>{title}</h1>
+//         {/* <ProjectsGrid projects={data.projects} /> */}
+//       </div>
+//     </Page>
+//   );
+// }
+
+export default function Projects() {
   return (
     <Page>
-      <div className="flex flex-col w-full h-full gap-8">
-        <h1>{title}</h1>
-        {/* <ProjectsGrid projects={data.projects} /> */}
-      </div>
+      <div>Projects</div>
     </Page>
   );
 }
